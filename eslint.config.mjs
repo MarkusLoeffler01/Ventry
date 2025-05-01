ade533a9d59bf3744b419ef3746720419af155c9
Markus Löffler <markus.loeffler01@gmail.com>
2025-05-01 21:05:33 +0200

    enhance ESLint configuration for TypeScript support and add ignore patterns

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import parser from "@typescript-eslint/parser";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,16 +11,43 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", "plugin:@typescript-eslint/recommended"),
   {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser,
+      parserOptions: {
+        project: "./tsconfig.json",
+        tsconfigRootDir: __dirname,
+        sourceType: "module",
+      },
+    },
     rules: {
-      "no-unused-vars": ["error", {
+      "@typescript-eslint/no-unused-vars": ["error", {
         argsIgnorePattern: "^_",
         varsIgnorePattern: "^_",
         caughtErrorsIgnorePattern: "^_",
+        ignoreRestSiblings: true,
       }],
+      "@typescript-eslint/no-misused-promises": "error"
     }
   }
 ];
 
-export default eslintConfig;
+const config = [
+  ...eslintConfig,
+  {
+    ignores: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/.next/**",
+      "**/out/**",
+      "**/build/**",
+      "**/coverage/**",
+      "**/public/**",
+      "**/src/generated/**"
+    ],
+  }
+]
+
+export default config;
